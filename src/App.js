d56f09e5cc869995b0047a48a86fe8daf03b88bf
Aditya giri <shashigiri04@gmail.com>
2024-04-24 16:18:57 +0530

    dewewd

--- src/App.js
+++ src/App.js
@@ -7,26 +7,30 @@ import React from 'react';
 
 
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-      <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="#home">Valo Nudes</Navbar.Brand>
-            <Nav className="me-auto">
-              <Link to="/" className='linker'>Home</Link>
-              <Link to="/artist" className='linker'>Artist</Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/artist" element={<Features />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+        function App() {
+          return (
+            <Router>
+              <div className="App" style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+                <Navbar bg="dark" data-bs-theme="dark">
+                  <Container>
+                    <Navbar.Brand href="#home">Valo Nudes</Navbar.Brand>
+                    <Nav className="me-auto">
+                      <Link to="/" className='linker'>Home</Link>
+                      <Link to="/artist" className='linker'>Artist</Link>
+                    </Nav>
+                  </Container>
+                </Navbar>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/artist" element={<Features />} />
+                </Routes>
 
-export default App;
+                <footer style={{ marginTop: 'auto', backgroundColor: 'black', padding: '10px', textAlign: 'center', color: 'white' }}>
+                  &copy; 2024 Beast Cut. All rights reserved.
+                </footer>
+              </div>
+            </Router>
+          );
+        }
+
+        export default App;
